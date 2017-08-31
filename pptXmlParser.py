49b2994632f225ae06f2ac67d1de039afb712313
mike persaud <michaelpersaud711@gmail.com>
2017-08-30 20:01:00 -0400

    updated to read produce matrix and di-graph

--- pptXmlParser.py
+++ pptXmlParser.py
@@ -1,50 +1,45 @@
 import xml.etree.ElementTree as etree
+import numpy as np
 
 tree = etree.parse('slide1.xml')
 root = tree.getroot()
-# print( root.tag)
-# print root.attrib
+
 #hardcoding the namespace
 p = "{http://schemas.openxmlformats.org/presentationml/2006/main}"
 a = "{http://schemas.openxmlformats.org/drawingml/2006/main}"
 #print p
 
-# for child in root:
-# 	print child.tag, child.attrib, child.text
-#
+###SCALE FACTOR = 12700
+SCALE_FACTOR = 12700
 
 spTree =tree.find('.//'+p+'spTree')
-#print node.tag
 
-#for child in node:
-#	print child
-#node = node.find(p+"spTree")
-
-#for child in spTree:
-#	print child.tag
-
-print
 shape_list = spTree.findall(p+'sp')
 cxn_list =  spTree.findall(p+'cxnSp')
 #debug purpose
 i = 0
+negative=0
+positive =0
+#counting nodes/rect
+r=0;
 
+##MATRIX && RECT/NODE MAP
+matrix = [[]]
+mapping = {}
+print
 #SHAPES LIST
 for child in shape_list:
 	#non-visual properties
 	shape = child.find('.//'+p+'cNvPr')
-
-	#print child.tag
-	#print child[0][0].attrib
-	#print child[1][1].attrib
-	#print shape
+	#shape properties
+	spPr = child.find(''+p+"spPr")
 
 	#PRINTS OUT RECTANGLES WITH ID AND NAME, OFFSET , WIDTH , HEIGHT
-	if child[1][1].attrib.get('prst') == "rect":
-	 	#print child[1][1].attrib
-		spPr = child.find(''+p+"spPr")
+	if child[1][1].attrib.get('prst') == "rect" or etree.iselement(spPr.find(a+"custGeom")) :
+
+
 		xfrm = spPr.find(a+"xfrm")
-		#print xfrm.attrib
+
 		x_offset= xfrm.find(a+'off').attrib.get('x')
 		y_offset= xfrm.find(a+'off').attrib.get('y')
 		width= xfrm.find(a+'ext').attrib.get('cx')
@@ -56,18 +51,24 @@ for child in shape_list:
 		for elem in t:
 			#print elem.text
 			full_text+="".join(elem.text)
-		print "|"+'id:' +shape.get('id') + " | name:"+shape.get('name')+"| Rectangle:"+full_text + "| x_offset: " + x_offset + "| y_offset:" + y_offset + "| width:" + width + "| height:" + height
-		#print child[3][2][1][1].text
+		#debugging purpose
+		#print "|"+'id:' +shape.get('id') + " | name:"+shape.get('name')+"| Rectangle:"+full_text + "| x_offset: " + x_offset + "| y_offset:" + y_offset + "| width:" + width + "| height:" + height
+
 		i=i+1
+		#add to map and increment node counter
+		mapping[int(shape.get('id'))]=r
+		print "Node "+str(r)+ ":"+full_text
+		r=r+1
 
 
 
+	# NOT NEEDED ANYMORE
 	#ARROWS CURVED DOWN or CURVED LEFT or Plus and Minu with ID AND NAME , OFFSET , WIDTH , HEIGHT
 	if child[1][1].attrib.get('prst') == "curvedDownArrow" or child[1][1].attrib.get('prst') == "curvedLeftArrow" or child[1][1].attrib.get('prst') == "mathPlus" or child[1][1].attrib.get('prst') == "mathMinus":
 		#print child[1][1].attrib
 		spPr = child.find(''+p+"spPr")
 		xfrm = spPr.find(a+"xfrm")
-		#print xfrm.attrib
+
 		x_offset= xfrm.find(a+'off').attrib.get('x')
 		y_offset= xfrm.find(a+'off').attrib.get('y')
 		width= xfrm.find(a+'ext').attrib.get('cx')
@@ -75,13 +76,39 @@ for child in shape_list:
 		print 'id:' +shape.get('id') + " | name:"+shape.get('name')+ "| x_offset: " + x_offset + "| y_offset:" + y_offset + "| width:" + width + "| height:" + height
 		i=i+1
 
+#initalize the matrix
+matrix = np.matrix([[0]*r]*r)
+
 #CONNECTORS LIST consisting of connector shapes <cxnSp>
 for child in cxn_list:
 	CxnSpPr = child.find('.//'+p+'nvCxnSpPr')
 	cNvPr = CxnSpPr.find('.//'+p+'cNvPr')
 	spPr = child.find(''+p+"spPr")
 	xfrm = spPr.find(a+"xfrm")
-	#print xfrm.attrib
+	aln = spPr.find(a+"ln")
+
+	#DEFAULT as Positive
+	RGB="+"
+	asolidFill = aln.find(a+"solidFill")
+	if(asolidFill!=None):
+		aRGB = asolidFill.find(a+"srgbClr")
+		if(aRGB!=None):
+			RGB = aRGB.get('val')
+			RGB="-"
+			negative=negative+1
+		else:
+			positive=positive+1
+	else:
+		RGB="+"
+		positive=positive+1
+
+
+	#DEAFULT IS SCALE_FACTOR if not grab value
+	line_width = aln.get('w')
+	if(line_width==None):
+		line_width='12700'
+
+
 	cNvCxnSpPr = CxnSpPr.find(p+'cNvCxnSpPr')
 	start_Cxn = cNvCxnSpPr.find(a+'stCxn')
 	if(etree.iselement(start_Cxn)):
@@ -100,8 +127,32 @@ for child in cxn_list:
 	y_offset= xfrm.find(a+'off').attrib.get('y')
 	width= xfrm.find(a+'ext').attrib.get('cx')
 	height= xfrm.find(a+'ext').attrib.get('cy')
-	print 'id:' +cNvPr.get('id') + " | name:"+cNvPr.get('name')+ "| x_offset: " + x_offset + "| y_offset:" + y_offset + "| width:" + width + "| height:" + height + "| Start Con:" + start_Cxn + "| End Con:" + end_Cxn
+	#print 'id:' +cNvPr.get('id') + "| Sign:"+RGB+"| name:"+cNvPr.get('name')+ "| x_offset: " + x_offset + "| y_offset:" + y_offset + "| width:" + width + "| height:" + height + "| Start Con:" + start_Cxn + "| End Con:" + end_Cxn +" | line_width: "+line_width
+	#change to mapping and input to matrix
+	if start_Cxn =='0' or end_Cxn =='0':
+		continue
+	start= mapping.get(int(start_Cxn))
+	end= mapping.get(int(end_Cxn))
+
+	if(RGB=='-'):
+		line_width=float(line_width)*-1.0
+	matrix[end].put(start,(float(line_width)*1.0))
 	i=i+1
 
 print
-print i
+print "Objects: "+str(i)
+print "Nodes: "+str(r)
+print "Negative Connects: "+str(negative)
+print "Positive Connects: "+str(positive)
+
+print "Node Mapping to ID: "+str(sorted( ((v,k) for k,v in mapping.iteritems()), reverse=False))
+print "--------------------------"
+print "Directed In-Graph"
+matrix =np.multiply(1.0/SCALE_FACTOR,matrix)
+matrix =np.matrix(np.round(matrix,3))
+print matrix
+print "-------------------------"
+print "Directed Out-Graph(Transpose)"
+
+print matrix.getT()
+print
